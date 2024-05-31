eb26898f8e961a26bbd216441b5fa2140314c953
zrobekm <150858924+zrobekm@users.noreply.github.com>
2024-05-31 23:56:21 +0200

    Python-03 completed

--- Session_3/module_3.py
+++ Session_3/module_3.py
@@ -5,34 +5,55 @@ Matrix = List[List[int]]
 
 
 def task_1(exp: int):
-    pass
+    def power(x):
+        powered = x**exp
+        print(powered)
+        return powered
+    return power
 
-
-def task_2(*args, **kwags):
-    pass
-
+def task_2(*args, **kwargs):
+    for arg in args:
+        print(arg)
+    for key, value in kwargs.items():
+        print(value)
 
 def helper(func):
-    pass
-
+    def wrapper(name):
+        print("Hi, friend! What's your name?")
+        func(name)
+        print("See you soon!")
+    return wrapper
 
 @helper
 def task_3(name: str):
     print(f"Hello! My name is {name}.")
 
-
 def timer(func):
-    pass
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__} in {run_time:.4f} secs")
+        return result
 
+    return wrapper
 
 @timer
 def task_4():
-    return len([1 for _ in range(0, 10**8)])
+    return len([1 for _ in range(0, 10 ** 8)])
+
 
 
 def task_5(matrix: Matrix) -> Matrix:
-    pass
+    num_rows = len(matrix)
+    num_columns = len(matrix[0])
 
+    Matrix = []
+    for j in range(num_columns):
+        new_row = []
+        for i in range(num_rows):
+            new_row.append(matrix[i][j])
+        Matrix.append(new_row)
 
-def task_6(queue: str):
-    pass
+    return Matrix
